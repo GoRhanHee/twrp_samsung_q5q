ef2ea670c5a445e8329e74e0cffaa38267cabc03
ravindu644 <droidcasts@protonmail.com>
2025-10-10 00:54:51 +0530

    twrp: include fastbootd mode
    Signed-off-by: ravindu644 <droidcasts@protonmail.com>

--- device.mk
+++ device.mk
@@ -9,3 +9,8 @@ LOCAL_PATH := device/samsung/q5q
 
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.1-impl-mock \
+    fastbootd
