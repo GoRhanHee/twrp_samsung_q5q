6d972fe29eba918ba8a3af9852fcc4ab0af1bc53
GoRhanHee <gorhanhee01@gmail.com>
2026-02-08 02:13:22 +0900

    Import F946NKSS5FZA1 TWRP device tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_q5q.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_q5q-user \
+    omni_q5q-userdebug \
+    omni_q5q-eng
